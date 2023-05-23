036c5e7ba3c03c0ae6910cc58863cff4650778fb
dv970406 <dv970406@gmail.com>
2023-05-23 23:41:46 +0900

    feat: Add Layout Header

--- frontend/src/components/templates/layout/Header.tsx
+++ frontend/src/components/templates/layout/Header.tsx
@@ -1,9 +1,40 @@
+import Link from "next/link";
 import React from "react";
+import { TbBinaryTree2, TbBrandReact, TbCategory } from "react-icons/tb";
 
+const NAVS_LIST = [
+  {
+    label: "소",
+    href: "introduction",
+  },
+  {
+    label: "채",
+    href: "recruit",
+  },
+  {
+    label: "서",
+    href: "service",
+  },
+  {
+    label: "Q",
+    href: "qna",
+  },
+];
 const Header = () => {
   return (
-    <header className="py-4 border-b-[1.5px] flex-center border-b-orange-300">
-      Header
+    <header className="h-14 fixed inset-x-0 bg-white z-50 border-b-[1.5px] flex-center border-b-danggn-orange px-2">
+      <div className="w-full flex-center between-center sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg">
+        <div></div>
+        <nav className="">
+          <ul className="gap-4 row-box">
+            {NAVS_LIST.map((nav) => (
+              <li className="text-danggn-orange">
+                <Link href={nav.href}>{nav.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
     </header>
   );
 };
