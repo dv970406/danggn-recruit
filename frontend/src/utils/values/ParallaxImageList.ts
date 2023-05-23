839915fd7f513084feace371ba6420e1e37724eb
dv970406 <dv970406@gmail.com>
2023-05-23 23:38:01 +0900

    feat: Add JSON Data setup

---
+++ frontend/src/utils/values/ParallaxImageList.ts
@@ -0,0 +1,122 @@
+export const PARALLAX_IMAGE_LIST = [
+  {
+    src: "/1.png",
+    alt: "danggn1",
+    position: {
+      top: 5,
+      left: 45,
+    },
+  },
+  {
+    src: "/2.png",
+    alt: "danggn2",
+    position: {
+      top: 19,
+      left: 40,
+    },
+  },
+  {
+    src: "/3.png",
+    alt: "danggn3",
+    position: {
+      top: 30,
+      left: 4,
+    },
+  },
+  {
+    src: "/4.png",
+    alt: "danggn4",
+    position: {
+      top: 13,
+      left: 6,
+    },
+  },
+  {
+    src: "/5.png",
+    alt: "danggn5",
+    position: {
+      top: 50,
+      left: 65,
+    },
+  },
+  {
+    src: "/6.png",
+    alt: "danggn6",
+    position: {
+      top: 32,
+      left: 40,
+    },
+  },
+  {
+    src: "/7.png",
+    alt: "danggn7",
+    position: {
+      top: 85,
+      left: 35,
+    },
+  },
+  {
+    src: "/8.png",
+    alt: "danggn8",
+    position: {
+      top: 75,
+      left: 70,
+    },
+  },
+  {
+    src: "/9.png",
+    alt: "danggn9",
+    position: {
+      top: 42,
+      left: 5,
+    },
+  },
+  {
+    src: "/10.png",
+    alt: "danggn10",
+    position: {
+      top: 55,
+      left: 6,
+    },
+  },
+  {
+    src: "/11.png",
+    alt: "danggn11",
+    position: {
+      top: 35,
+      left: 70,
+    },
+  },
+  {
+    src: "/12.png",
+    alt: "danggn12",
+    position: {
+      top: 15,
+      left: 70,
+    },
+  },
+  {
+    src: "/13.png",
+    alt: "danggn13",
+    position: {
+      top: 45,
+      left: 35,
+    },
+  },
+  {
+    src: "/14.png",
+    alt: "danggn14",
+    position: {
+      top: 75,
+      left: 4,
+    },
+  },
+  {
+    src: "/15.png",
+    alt: "danggn15",
+    position: {
+      top: 65,
+      left: 35,
+    },
+  },
+];
---
+++ frontend/src/utils/values/RecruitPartList.ts
@@ -0,0 +1,32 @@
+export const RECRUIT_PART_LIST = [
+  {
+    part: "frontend",
+    label: "FE",
+    img: "",
+    desc: "",
+  },
+  {
+    part: "backend",
+    label: "BE",
+    img: "",
+    desc: "",
+  },
+  {
+    part: "iOS",
+    label: "iOS",
+    img: "",
+    desc: "",
+  },
+  {
+    part: "marketing",
+    label: "Marketing",
+    img: "",
+    desc: "",
+  },
+  {
+    part: "pm",
+    label: "PM",
+    img: "",
+    desc: "",
+  },
+];
