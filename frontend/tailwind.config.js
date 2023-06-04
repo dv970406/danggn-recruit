60f3808d93197686e73b7d67019e983b6ecb1aec
dv970406 <dv970406@gmail.com>
2023-06-04 16:43:33 +0900

    style: Add tailwind palette

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -10,8 +10,54 @@ module.exports = {
       fontFamily: {
         montserrat: ["Montserrat", "Noto Sans KR", "sans-serif"],
       },
-      colors: {},
+      colors: {
+        danggn: {
+          orange: "#FF8A3D",
+          darkgray: "#868B94",
+          lightgray: "#EAEBEE",
+        },
+      },
+      keyframes: {
+        "appear-right-to-left": {
+          from: {
+            opacity: 0,
+            transform: "translateX(100%)",
+          },
+          to: {
+            opacity: 1,
+            transform: "translateX(0)",
+          },
+        },
+        "appear-left-to-right": {
+          from: {
+            opacity: 0,
+            transform: "translateX(-100%)",
+          },
+          to: {
+            opacity: 1,
+            transform: "translateX(0)",
+          },
+        },
+        "appear-bottom-to-top": {
+          from: {
+            opacity: 0,
+            transform: "translateY(100%)",
+          },
+          to: {
+            opacity: 1,
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "appear-right-to-left":
+          ".75s appear-right-to-left ease-in-out forwards",
+        "appear-left-to-right":
+          ".75s appear-left-to-right ease-in-out forwards",
+        "appear-bottom-to-top":
+          ".75s appear-bottom-to-top ease-in-out forwards",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
