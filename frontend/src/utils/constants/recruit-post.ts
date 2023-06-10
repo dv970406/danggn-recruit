7e3985b8f7aeed8003da51a492c1a22216253b79
dv970406 <dv970406@gmail.com>
2023-06-10 12:00:55 +0900

    feat: Add utils/constants

--- frontend/src/utils/constants/recruit-post.ts
+++ frontend/src/utils/constants/recruit-post.ts
@@ -1,1 +1,1 @@
-export const INIT_RECRUIT_POSTS_QUERY_STRING = `keyword=${""}&careerType=all&partName=Frontend`;
+export const INIT_RECRUIT_POSTS_QUERY_STRING = `keyword=${""}&careerType=all&partName=프론트엔드&pageParam=1`;
