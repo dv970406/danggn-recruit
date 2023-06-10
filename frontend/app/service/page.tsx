820a0c168921c7c2982ddda55c4fcd351ab03cbf
dv970406 <dv970406@gmail.com>
2023-06-10 12:25:59 +0900

    feat: Add ServicePage
    - 3D를 활용한 당근마켓의 서비스 소개 페이지

--- frontend/app/service/page.tsx
+++ frontend/app/service/page.tsx
@@ -1,17 +1,20 @@
-import CanvasWrapper from "@/src/components/templates/service/CanvasWrapper";
-import ServiceCanvas from "@/src/components/templates/service/ServiceCanvas";
+import Service3DCanvas from "@/src/components/templates/service/Service3DCanvas";
 import ServiceChoice from "@/src/components/templates/service/ServiceChoice";
 import ServiceDescription from "@/src/components/templates/service/ServiceDescription";
+import CanvasWrapper from "@/src/components/templates/shared/CanvasWrapper";
 import React from "react";
 
 export const metadata = {
   title: "서비스",
 };
+
+// /service Page 렌더링 방식 : SSG
+// 변동되는 데이터 없음
 const ServicePage = () => {
   return (
     <article className="relative layout flex-center column-box">
       <CanvasWrapper>
-        <ServiceCanvas />
+        <Service3DCanvas />
       </CanvasWrapper>
       <ServiceDescription />
       <ServiceChoice />
