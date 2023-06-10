c73d108783fe32d92e73e43c0da2058913b95e8d
dv970406 <dv970406@gmail.com>
2023-06-10 12:21:07 +0900

    chore: Add 3d packages & etc packages
    - 3D 관련 라이브러리: threejs, matter-js
    - data fetching 라이브러리 : react-query
    - 3D 애니메이션 라이브러리 : gsap
    - 유틸 라이브러리 : react-icons, react-spinners, markdown, carousel
    - 전역 상태관리 라이브러리 : recoil

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      // 외부 이미지 출처 도메인 등록
+      { protocol: "https", hostname: "user-images.githubusercontent.com" },
+    ],
+  },
+};
 
 module.exports = nextConfig;
