ca2f12de5f370f519f2f1745b6b3cf87901ab424
dv970406 <dv970406@gmail.com>
2023-06-13 01:51:08 +0900

    feat: Modify res.cookie domain setup

--- backend/src/applicant/applicant.controller.ts
+++ backend/src/applicant/applicant.controller.ts
@@ -25,8 +25,8 @@ export class ApplicantController {
         httpOnly: true,
         maxAge: 24 * 60 * 60 * 1000, // 하루
         sameSite: 'none', // 백엔드 배포한 서비스와 프론트엔드 배포한 서비스가 다름
+        domain: 'danggn-recruit.vercel.app',
       });
-      console.log('tokne 31 : ', token);
       return res.status(200).json({ ok });
     } else {
       return res.status(404).json({ ok, error });
--- frontend/app/resume/fetcher.ts
+++ frontend/app/resume/fetcher.ts
@@ -6,7 +6,6 @@ import { cache } from "react";
 export const getAppliedRecruitPosts = cache(async () => {
   const token = getTokenInCookie();
 
-  console.log("is in token ? :", token);
   const response = await fetch(process.env.SERVER_URL + `/resume/my`, {
     cache: "no-cache",
     // SSR하에서는 쿠키에 들어있는 토큰을 직접 넣어줘야함(브라우저 측에 저장된 쿠키에 접근할 수 없기 때문)
