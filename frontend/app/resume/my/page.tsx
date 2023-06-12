6be1d10486219a6c5e9b332f615093c89e59a879
dv970406 <dv970406@gmail.com>
2023-06-13 00:22:23 +0900

    docs: Modify use client in MyResumes

--- frontend/app/resume/my/page.tsx
+++ frontend/app/resume/my/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata, NextPage } from "next";
 import React, { Suspense } from "react";
-import { getAppliedRecruitPosts } from "../fetcher";
 import MyResumes from "@/src/components/templates/resume/MyResumes";
 import Loader from "@/src/components/atomics/Loader";
 
@@ -13,6 +12,7 @@ interface IMyResumePage {}
 // /resume/my Page 렌더링 방식 : CSR
 // 인증한 유저에 따라 다른 데이터를 보여줘야함. - 미리 만들어두는 것 불가(=SSG불가)
 // 유저의 정보가 필요하다보니 generateStatic으로 정적으로 만들어두는 것도 사실상 불가
+// SEO가 중요한 페이지도 아님 => SSR 배제
 // 유저가 인증하기 버튼을 눌러 이동하는 페이지이다보니 CSR이 적당할듯
 const MyResumePage: NextPage<IMyResumePage> = ({}) => {
   // Suspense 활용을 위해 Promise는 자식 컴포넌트에서 await할 것
--- frontend/src/components/templates/resume/MyResumes.tsx
+++ frontend/src/components/templates/resume/MyResumes.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import Title from "../../atomics/Title";
 import MyResumeItem from "../../organisms/resume/MyResumeItem";
