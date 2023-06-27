126a7dea7d013b421f94b058b3ea6113f1d7fd44
dv970406 <dv970406@gmail.com>
2023-06-27 13:45:20 +0900

    refactor: Modify ListWrapper 모듈화
    - 공통적으로 사용되는 ul의 style의 반복작성을 줄이기 위한 컴포넌트 모듈화

--- frontend/src/components/templates/resume/MyResumes.tsx
+++ frontend/src/components/templates/resume/MyResumes.tsx
@@ -4,6 +4,7 @@ import { IApplicant } from "@/src/type/applicant.interface";
 import Title from "../../atomics/Title";
 import MyResumeItem from "../../organisms/resume/MyResumeItem";
 import NoData from "../../organisms/shared/NoData";
+import ListWrapper from "../../molecules/boxes/ListWrapper";
 
 interface IMyResumes {
   appliedRecruitPostsPromise: Promise<{
@@ -19,13 +20,13 @@ const MyResumes = async ({ appliedRecruitPostsPromise }: IMyResumes) => {
   return (
     <section className="w-full gap-4 mt-8 column-box">
       <Title text={`${myInfo.name}님의 지원내역`} />
-      <ul className="w-full gap-4 column-box">
+      <ListWrapper>
         {myResumes.map((myResume) => (
           <MyResumeItem myResume={myResume} />
         ))}
         {/* 데이터가 있어야 지원자 정보가 있기 때문에 NoData인 상황이 존재할 수 없긴 함 */}
         {myResumes.length === 0 && <NoData />}
-      </ul>
+      </ListWrapper>
     </section>
   );
 };
